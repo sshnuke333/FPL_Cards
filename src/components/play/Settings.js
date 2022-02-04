6223a5444e6482ca4a08ba3eba98d53dc573eeb7
sshnuke333 <sshnuke@protonmail.com>
2022-02-05 03:10:01 +0530

    added button to change deck

--- src/components/play/Settings.js
+++ src/components/play/Settings.js
@@ -1,5 +1,54 @@
 import React from 'react';
+import { Div, Button, Form, Gear } from './Settings.styles';
+
+const displayForm = (e) => {
+    let attribute = 'aria-expanded';
+    let expanded = e.target.getAttribute(attribute);
+    let form = document.getElementById('settings');
+    expanded === 'false'
+        ? e.target.setAttribute(attribute, 'true')
+        : e.target.setAttribute(attribute, 'false');
+    expanded === 'false'
+        ? (form.style.display = 'block')
+        : (form.style.display = '');
+};
+
+const updateDisplay = (e) => {
+    document.querySelector('output').value = `${e.target.value} Cards`;
+};
 
 export const Settings = () => {
-    return <div data-testid="settings"></div>;
+    return (
+        <Div data-testid="settings">
+            <Button
+                aria-label="settings"
+                aria-expanded="false"
+                onClick={displayForm}
+            >
+                <Gear size={24} style={{ PointerEvent: 'none' }} />
+            </Button>
+            <Form aria-label="options" id="settings">
+                <legend style={{ paddingBottom: '0.5rem' }}>
+                    choose deck size:
+                </legend>
+                <input
+                    type="range"
+                    aria-label="slider"
+                    id="deck"
+                    name="deck"
+                    min="5"
+                    max="11"
+                    step="2"
+                    onInput={updateDisplay}
+                />
+                <output
+                    name="result"
+                    htmlFor="deck"
+                    style={{ display: 'block', paddingTop: '0.5rem' }}
+                >
+                    11 Cards
+                </output>
+            </Form>
+        </Div>
+    );
 };
--- src/components/play/Settings.test.js
+++ src/components/play/Settings.test.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { userEvent } from '@testing-library/user-event';
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Settings } from './Settings';
 
-describe('Settings component', () => {
+describe('Settings component renders', () => {
     beforeEach(() => {
         render(<Settings />);
     });
-    it('renders', () => {});
+    it('a button', () => {
+        expect(
+            screen.getByRole('button', { name: /settings/i })
+        ).toBeInTheDocument();
+    });
+    it('a hidden options div', () => {
+        expect(screen.getByLabelText(/options/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/options/i)).not.toBeVisible();
+    });
+    it('options has a range input in it', () => {
+        const form = screen.getByLabelText(/options/i);
+        expect(within(form).getByLabelText(/slider/i)).toBeInTheDocument();
+    });
+});
+describe('button', () => {
+    beforeEach(() => {
+        render(<Settings />);
+    });
+    it('is not expanded', () => {
+        expect(
+            screen.getByRole('button', { expanded: false })
+        ).toBeInTheDocument();
+    });
+    it('on clicking displays form', () => {
+        userEvent.click(screen.getByRole('button', { name: /settings/i }));
+        expect(screen.getByLabelText(/options/i)).toBeVisible();
+    });
+    it('on clicking again hides form', () => {
+        userEvent.dblClick(screen.getByRole('button', { name: /settings/i }));
+        expect(screen.getByLabelText(/options/i)).not.toBeVisible();
+    });
 });
