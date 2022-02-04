2fac1e35db44167a1706db05d28c42ad84568cbd
sshnuke333 <sshnuke@protonmail.com>
2022-02-05 03:10:42 +0530

    added styles

---
+++ src/components/play/Settings.styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import { GearIcon } from '@primer/octicons-react';
+
+export const Div = styled.div`
+    width: 20rem;
+    height: 10rem;
+    position: absolute;
+    left: 5rem;
+    text-align: end;
+`;
+
+export const Button = styled.button`
+    width: 2rem;
+    height: 2rem;
+    background-color: transparent;
+    margin: 0.5rem;
+    cursor: pointer;
+`;
+
+export const Gear = styled(GearIcon)`
+    size: 1.5rem;
+    pointer-events: none;
+`;
+
+export const Form = styled.form`
+    display: none;
+    padding: 1rem;
+`;
