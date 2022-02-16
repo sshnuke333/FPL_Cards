11466e5349438afd0367df4b3eac99b6b7a1903b
sshnuke333 <sshnuke@protonmail.com>
2022-02-17 02:43:17 +0530

    added svg component as background

--- src/index.styles.js
+++ src/index.styles.js
@@ -1,4 +1,12 @@
+import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
+import { renderToStaticMarkup } from 'react-dom/server';
+import { Background } from './components/Background';
+const svgBackground = encodeURIComponent(
+    renderToStaticMarkup(
+        <Background gradientStart={'#37003c'} gradientEnd={'#963cff'} />
+    )
+);
 
 export const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;1,400&display=swap');
@@ -18,5 +26,9 @@ li{
 }
 a {
 	text-decoration: none;
+}
+body{
+	background-image: ${`url("data:image/svg+xml,${svgBackground}"),linear-gradient(270deg, #963cff, #37003c)`};
+	background-size: cover;
 }
 `;
