db94572efbbf761c8d52740c9dccc86b3a6248e3
sshnuke333 <sshnuke@protonmail.com>
2022-02-25 00:42:20 +0530

    changes: * added new state 'populated' * removed setCards and added additional reducers * added thunk functions to batch dispatch multiple state changes

--- src/store/Cards.slice.js
+++ src/store/Cards.slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { batch } from 'react-redux';
 
 export const initialState = {
     deckSize: 22,
@@ -6,16 +7,16 @@ export const initialState = {
     opponentCards: [],
     currentPlayerCard: {},
     currentOpponentCard: {},
+    populated: false,
     gameOver: false,
 };
+
+// directly mutating state: createSlice has immer
 
 const cardsSlice = createSlice({
     name: 'cards',
     initialState,
     reducers: {
-        setCards: (state) => {
-            state.loading = true;
-        },
         setDeckSize: (state, { payload }) => {
             state.deckSize = payload;
         },
@@ -31,18 +32,75 @@ const cardsSlice = createSlice({
         setCurrentOpponentCard: (state, { payload }) => {
             state.currentOpponentCard = { ...payload };
         },
+        setPopulated: (state) => {
+            state.populated = !state.populated;
+        },
+        updatePlayerCards: (state) => {
+            state.playerCards.shift();
+        },
+        updateOpponentCards: (state) => {
+            state.opponentCards.shift();
+        },
+        setGameOver: (state) => {
+            state.gameOver = !state.gameOver;
+        },
+        resetGameState: () => {
+            return initialState;
+        },
     },
 });
 
 export const {
-    setCards,
     setDeckSize,
     setPlayerCards,
     setOpponentCards,
     setCurrentPlayerCard,
     setCurrentOpponentCard,
+    setPopulated,
+    updatePlayerCards,
+    updateOpponentCards,
+    setGameOver,
+    resetGameState,
 } = cardsSlice.actions;
 
 export const cardsSelector = (state) => state.cards;
 
 export default cardsSlice.reducer;
+
+// Thunk functions to dispatch multiple actions
+// built in batch() to render single UI change for multiple state changes
+export function resultObtained(
+    player,
+    opponent,
+    playerCard,
+    opponentCard,
+    result
+) {
+    return (dispatch) => {
+        batch(() => {
+            if (result === undefined) {
+                dispatch(setPlayerCards(player));
+                dispatch(setOpponentCards(opponent));
+            } else if (result) {
+                dispatch(setPlayerCards(player));
+                dispatch(setPlayerCards(opponent));
+            } else {
+                dispatch(setOpponentCards(opponent));
+                dispatch(setOpponentCards(player));
+            }
+            dispatch(setCurrentPlayerCard(playerCard));
+            dispatch(setCurrentOpponentCard(opponentCard));
+            dispatch(updatePlayerCards());
+            dispatch(updateOpponentCards());
+        });
+    };
+}
+export function gameEnd() {
+    return (dispatch) => {
+        batch(() => {
+            dispatch(setPopulated());
+            dispatch(resetGameState());
+            dispatch(setGameOver());
+        });
+    };
+}
