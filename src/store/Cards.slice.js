d7fee12d463cb8cb297e35cd900234ed1a3b1694
sshnuke333 <sshnuke@protonmail.com>
2022-02-26 01:08:22 +0530

    changes: * resetGameState now retains deckSize and gameOver state * refactored resultObtained thunk to three different thunk functions * gameEnd thnk doesn't change populated value

--- src/store/Cards.slice.js
+++ src/store/Cards.slice.js
@@ -44,8 +44,12 @@ const cardsSlice = createSlice({
         setGameOver: (state) => {
             state.gameOver = !state.gameOver;
         },
-        resetGameState: () => {
-            return initialState;
+        resetGameState: (state) => {
+            return {
+                ...initialState,
+                deckSize: state.deckSize,
+                gameOver: state.gameOver,
+            };
         },
     },
 });
@@ -69,36 +73,89 @@ export default cardsSlice.reducer;
 
 // Thunk functions to dispatch multiple actions
 // built in batch() to render single UI change for multiple state changes
-export function resultObtained(
-    player,
-    opponent,
-    playerCard,
-    opponentCard,
-    result
-) {
+
+// if player card has higher value he wins the opponent card and retains his card
+export function cardWin(player, opponent, nextPlayerCard, nextOpponentCard) {
+    return (dispatch) => {
+        // opponent card is undefined if has only 1 card
+        // opponent loses the game
+        if (opponentCard === undefined) dispatch(gameEnd());
+        else {
+            batch(() => {
+                // add current opponent card to player cards pile
+                // add next opponent card as his current card and remove that card from state
+                dispatch(setPlayerCards(opponent));
+                dispatch(setCurrentOpponentCard(nextOpponentCard));
+                dispatch(updateOpponentCards());
+                if (playerCard !== undefined) {
+                    // add current player card to player cards pile
+                    // add next opponent card as his current card and remove that card from state
+                    dispatch(setPlayerCards(player));
+                    dispatch(setCurrentPlayerCard(nextPlayerCard));
+                    dispatch(updatePlayerCards());
+                }
+            });
+        }
+    };
+}
+// if player card has lower value player loses his card to opponent
+export function cardLoss(player, opponent, nextPlayerCard, nextOpponentCard) {
+    return (dispatch) => {
+        // player card is undefined if has only 1 card
+        // player loses the game
+        if (playerCard === undefined) dispatch(gameEnd());
+        else {
+            batch(() => {
+                // add current player card to opponent cards pile
+                // add next player card as his current card and remove that card from state
+                dispatch(setOpponentCards(player));
+                dispatch(setCurrentPlayerCard(nextPlayerCard));
+                dispatch(updatePlayerCards());
+                if (opponentCard !== undefined) {
+                    // add current opponent card to his cards pile
+                    // add next opponent card as his current card and remove that card from state
+                    dispatch(setOpponentCards(opponent));
+                    dispatch(setCurrentOpponentCard(nextOpponentCard));
+                    dispatch(updateOpponentCards());
+                }
+            });
+        }
+    };
+}
+// if both cards have same value both retain their cards
+export function cardDraw(player, opponent, nextPlayerCard, nextopponentCard) {
     return (dispatch) => {
         batch(() => {
-            if (result === undefined) {
-                dispatch(setPlayerCards(player));
+            // player card is undefined if has only 1 card and retains it
+            // add current opponent card to his cards pile, add his next card as his current card and remove that card from state
+            if (playerCard === undefined) {
                 dispatch(setOpponentCards(opponent));
-            } else if (result) {
-                dispatch(setPlayerCards(player));
-                dispatch(setPlayerCards(opponent));
-            } else {
-                dispatch(setOpponentCards(opponent));
-                dispatch(setOpponentCards(player));
+                dispatch(setCurrentOpponentCard(nextOpponentCard));
+                dispatch(updateOpponentCards());
             }
-            dispatch(setCurrentPlayerCard(playerCard));
-            dispatch(setCurrentOpponentCard(opponentCard));
-            dispatch(updatePlayerCards());
-            dispatch(updateOpponentCards());
+            // opponent card is undefined if has only 1 card and retains it
+            // add current player card to his cards pile, add his next card as his current card and remove that card from state
+            else if (opponentCard === undefined) {
+                dispatch(setPlayerCards(player));
+                dispatch(setCurrentPlayerCard(nextPlayerCard));
+                dispatch(updatePlayerCards());
+            }
+            // both have more than 1 card
+            // both retain their cards and move to their next cards
+            else {
+                dispatch(setPlayerCards(player));
+                dispatch(setOpponentCards(opponent));
+                dispatch(setCurrentPlayerCard(nextPlayerCard));
+                dispatch(setCurrentOpponentCard(nexOpponentCard));
+                dispatch(updatePlayerCards());
+                dispatch(updateOpponentCards());
+            }
         });
     };
 }
 export function gameEnd() {
     return (dispatch) => {
         batch(() => {
-            dispatch(setPopulated());
             dispatch(resetGameState());
             dispatch(setGameOver());
         });
