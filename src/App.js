060bbd118eb38164aab8e73b4328fb2194d090de
sshnuke333 <sshnuke@protonmail.com>
2022-02-28 18:22:38 +0530

    added home element to route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Disclaimer } from './view/Disclaimer';
 import { Header } from './view/Header';
-import { Play } from './view/Play';
+import { Home } from './view/home/Home';
+import { Play } from './view/play/Play';
+import { Disclaimer } from './view/Disclaimer';
 import { NotFound } from './view/NotFound';
 export default function App() {
     return (
         <Routes>
             <Route path="/" element={<Header />}>
+                <Route path="/" element={<Home />} />
                 <Route path="play" element={<Play />} />
                 <Route path="disclaimer" element={<Disclaimer />} />
                 <Route path="*" element={<NotFound />} />
